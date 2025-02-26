94b4b1f2e2e651ce0844d04064e379e06d4636e3
nandanathaniela <natnanda04@gmail.com>
2025-02-26 20:44:33 +0700

    feat: [GREEN] edit button.tsx to pass the test

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,17 @@
 'use client';
 
-const Button = () => <button></button>;
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => (
+  <button
+    onClick={onClick}
+    className="btn-primary w-full rounded-2xl h-12 mt-4"
+  >
+    {text}
+  </button>
+);
 
 export default Button;
