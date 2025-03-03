e732e3cd53c3a39558b48a05cf4a7c777d02ff4d
unknown <shabhi3101@gmail.com>
2025-03-03 23:00:01 +0700

    feat(login): [GREEN] add onClick function to Button component

--- src/components/modules/login/components/Button.tsx
+++ src/components/modules/login/components/Button.tsx
@@ -1,12 +1,17 @@
-'use client';
+('use client');
 
 interface ButtonProps {
   text: string;
   onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = () => (
-  <button className="btn-primary w-full rounded-2xl h-12 mt-4"></button>
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => (
+  <button
+    onClick={onClick}
+    className="btn-primary w-full rounded-2xl h-12 mt-4"
+  >
+    {text}
+  </button>
 );
 
 export default Button;
