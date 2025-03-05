3f94f23bb0879e46e953acd94a76db1140757e14
unknown <shabhi3101@gmail.com>
2025-03-05 11:33:38 +0700

    feat(login): [RED] add test for Auth Tabs component

---
+++ src/components/modules/login/components/AuthTabs.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+interface AuthTabsProps {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+}
+
+const AuthTabs: React.FC<AuthTabsProps> = ({}) => {
+  return <></>;
+};
+
+export default AuthTabs;
