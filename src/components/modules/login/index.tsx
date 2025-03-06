f4dc90b2dce9b380efdb346016f7b8f49f074c75
unknown <shabhi3101@gmail.com>
2025-03-06 09:05:50 +0700

    feat: add LoginModule to index.tsx for login page

--- src/components/modules/login/index.tsx
+++ src/components/modules/login/index.tsx
@@ -1,5 +1,22 @@
 'use client';
 
+import { useState } from 'react';
+import AuthTabs from './components/AuthTabs';
+import LoginForm from './components/LoginForm';
+import Logo from './components/Logo';
+
 export function LoginModule() {
-  return <></>;
+  const [activeTab, setActiveTab] = useState('LOGIN');
+
+  return (
+    <div className="flex min-h-screen items-center justify-center  bg-white px-4">
+      <div className="w-full max-w-md bg-white p-6 rounded-lg ">
+        <div className="flex flex-col items-center mb-8">
+          <Logo />
+        </div>
+        <AuthTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        {activeTab === 'LOGIN' && <LoginForm />}
+      </div>
+    </div>
+  );
 }
