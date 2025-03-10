7f6555ee15b0930ef68bee2e9670aafc69b36f36
sabiasa <shabhi3101@gmail.com>
2025-03-10 11:10:39 +0700

    chore: remove unused comment

--- src/components/core/hooks/use-avento-client.ts
+++ src/components/core/hooks/use-avento-client.ts
@@ -3,18 +3,5 @@
 import { aventoClient } from '@/lib';
 
 export const useAventoClient = () => {
-  // const { data: session } = useSession();
-
-  // useEffect(() => {
-  //   // Set the Authorization header when session changes
-  //   if (session?.accessToken) {
-  //     aventoClient.defaults.headers.common['Authorization'] =
-  //       `Bearer ${session.accessToken}`;
-  //   } else {
-  //     // Remove the Authorization header if no session
-  //     delete aventoClient.defaults.headers.common['Authorization'];
-  //   }
-  // }, [session]);
-
   return aventoClient;
 };
