97d57270c48c0b876c72848df94753ccecaa3a50
CyberSleeper <mahartha.gemilang@gmail.com>
2025-03-19 14:28:55 +0700

    chores: linting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from '@/components/ui/sonner';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -33,7 +33,7 @@ export default function RootLayout({
         {children}
         <Toaster />
         <SpeedInsights />
-        <Analytics/>
+        <Analytics />
       </body>
     </html>
   );
