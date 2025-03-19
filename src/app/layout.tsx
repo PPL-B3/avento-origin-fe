7695d4ade1af7364eab65cff2dc1695f7df1f0a2
CyberSleeper <mahartha.gemilang@gmail.com>
2025-03-19 14:26:46 +0700

    feat: add vercel analytcis monitoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from '@/components/ui/sonner';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -32,6 +33,7 @@ export default function RootLayout({
         {children}
         <Toaster />
         <SpeedInsights />
+        <Analytics/>
       </body>
     </html>
   );
