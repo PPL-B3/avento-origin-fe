b731aec5d4053ec92063dbb0dd8f4eed4f3906ba
CyberSleeper <mahartha.gemilang@gmail.com>
2025-03-28 14:26:37 +0700

    feat: add user info to logging

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useAuth } from '@/components';
 import LogRocket from 'logrocket';
 import { ReactNode, useEffect } from 'react';
 
@@ -8,15 +9,17 @@ interface ProvidersProps {
 }
 
 export function LogRocketProviders({ children }: ProvidersProps) {
+  const { user } = useAuth();
+
   useEffect(() => {
     if (process.env.LOGROCKET_ID) {
       LogRocket.init(process.env.LOGROCKET_ID);
+      console.log(process.env.LOGROCKET_ID);
 
       // Identify users if you have authentication
-      // LogRocket.identify('user-id', {
-      //   name: 'User Name',
-      //   email: 'user@example.com',
-      // });
+      LogRocket.identify('user-id', {
+        email: user?.email || '',
+      });
     } else {
       console.warn('LogRocket ID not found. LogRocket initialization skipped.');
     }
