09f973256696b10a511d9e885cf3da74cf5eef32
CyberSleeper <mahartha.gemilang@gmail.com>
2025-04-04 11:32:46 +0700

    feat: add playwright login flow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig, devices } from '@playwright/test';
+import { PlaywrightTestConfig } from '@playwright/test';
 
-export default defineConfig({
-  testDir: './tests/playwright',
+const config: PlaywrightTestConfig = {
+  testDir: './tests/playwright/specs',
+  timeout: 30000,
+  expect: {
+    timeout: 5000
+  },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -9,28 +13,19 @@ export default defineConfig({
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
+    actionTimeout: 0,
     trace: 'on-first-retry',
-    headless: false,
-    screenshot: 'only-on-failure',
     video: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        browserName: 'chromium',
+      },
     },
   ],
-  webServer: {
-    command: 'pnpm dev',
-    port: 3000,
-    reuseExistingServer: !process.env.CI,
-  },
-});
+};
+
+export default config;
