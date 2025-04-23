c2cd9d0690e57100296b3d077417d57a4a9f28d1
CyberSleeper <mahartha.gemilang@gmail.com>
2025-04-23 19:53:00 +0700

    feat: add filter component

--- src/components/modules/audit-log/list/elements/audit-log-table-columns-def.tsx
+++ src/components/modules/audit-log/list/elements/audit-log-table-columns-def.tsx
@@ -56,7 +56,20 @@ export function fetchAuditLogTableColumnDefs(): ColumnDef<
 }
 
 /* istanbul ignore next */
-export const filterableColumns: DataTableFilterableColumn<AuditLogEntry>[] = [];
+export const filterableColumns: DataTableFilterableColumn<AuditLogEntry>[] = [
+  {
+    id: 'eventType',
+    title: 'Event Type',
+    options: [
+      { label: 'Login', value: 'login' },
+      { label: 'Logout', value: 'logout' },
+      { label: 'Create', value: 'create' },
+      { label: 'Update', value: 'update' },
+      { label: 'Delete', value: 'delete' },
+      { label: 'View', value: 'view' },
+    ],
+  },
+];
 
 /* istanbul ignore next */
 export const searchableColumns: DataTableSearchableColumn<AuditLogEntry>[] = [];
