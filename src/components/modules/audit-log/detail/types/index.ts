b85d0b395fb3d3ee24f0db2599b1d16d60ddbb5b
nandanathaniela <natnanda04@gmail.com>
2025-04-26 15:45:46 +0700

    feat: add admin log document type

--- src/components/modules/audit-log/detail/types/index.ts
+++ src/components/modules/audit-log/detail/types/index.ts
@@ -2,3 +2,13 @@ export interface HistoryType {
   owner: string;
   generatedDate: string;
 }
+
+export interface DocumentAdminMetadataResponse {
+  documentId: string;
+  documentName: string;
+  uploadDate: string;
+  publisher: string;
+  currentOwner: string;
+  ownershipHistory: HistoryType[];
+  filePath?: string;
+}
